893c9c98f584b042958cddde4ba352d84c379c25
D. Pavlova <pavlova_desislava@outlook.com>
2023-03-02 22:45:29 +0200

    first upload

---
+++ Fundamentals/7.1. Dictionaries - Lab/statistics_03.py
@@ -0,0 +1,21 @@
+inventory = {}
+
+while True:
+    products = input().split(': ')
+
+    if products[0] == 'statistics':
+        break
+
+    product = products[0]
+    quantity = products[1]
+
+    if product not in inventory:
+        inventory[product] = int(quantity)
+    else:
+        inventory[product] += int(quantity)
+
+print("Products in stock:")
+for (product, quantity) in inventory.items():
+    print(f"- {product}: {quantity}")
+print(f"Total Products: {len(inventory)}")
+print(f"Total Quantity: {sum(inventory.values())}")
