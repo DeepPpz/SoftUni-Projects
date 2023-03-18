c65a6d7c710d555afab7b1a8340919c30e9949d8
Desislava Pavlova <pavlova_desislava@outlook.com>
2023-03-18 22:07:40 +0200

    Optimized code

--- Fundamentals/9.1. Regular Expressions - Lab/match_phone_number_02.py
+++ Fundamentals/9.1. Regular Expressions - Lab/match_phone_number_02.py
@@ -2,6 +2,6 @@ import re
 
 data = input()
 
-pattern = r"\+359\s2\s[0-9][0-9][0-9]\s[0-9][0-9][0-9][0-9]|\+359-2-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]\b"
+pattern = r"\+359\s2\s[0-9]{3}\s[0-9]{4}|\+359-2-[0-9]{3}-[0-9]{4}\b"
 phones = re.findall(pattern, data)
 print(*phones, sep=", ")
