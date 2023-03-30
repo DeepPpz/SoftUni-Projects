ed9f141d32118a6c9e55bd85d0e3986aa70e9111
Desislava Pavlova <pavlova_desislava@outlook.com>
2023-03-30 14:57:27 +0300

    Added new way of multiplying

--- Fundamentals/Final Exam Prep/05. Final Exam/emoji_detector_02.py
+++ Fundamentals/Final Exam Prep/05. Final Exam/emoji_detector_02.py
@@ -2,14 +2,15 @@ import re
 emoji_pattern = r"(::|\*\*)[A-Z][a-z]{2,}\1"
 
 data = input()
-cool_threshold = 1
-emoji_counter = 0
-cool_emojis = []
 
+# cool_threshold = math.prod([int(x) for x in data if x.isdigit()])
+cool_threshold = 1
 for ch in data:
     if ch.isdigit():
         cool_threshold *= int(ch)
 
+emoji_counter = 0
+cool_emojis = []
 emojis = re.finditer(emoji_pattern, data)
 
 for emoji in emojis:
