fcb0a3c551a36bd0ab9b559a6e97775428cc1a4a
Desislava Pavlova <pavlova_desislava@outlook.com>
2023-03-30 17:56:37 +0300

    Updated solution

--- Fundamentals/4.3. Functions - More Exercise/data_types_01.py
+++ Fundamentals/4.3. Functions - More Exercise/data_types_01.py
@@ -2,7 +2,7 @@ def define_type(a, b):
     if a == 'int':
         print(int(b) * 2)
     elif a == 'real':
-        print(f'{int(b) * 1.5:.2f}')
+        print(f'{float(b) * 1.5:.2f}')
     elif a == 'string':
         print(f'${b}$')
 
