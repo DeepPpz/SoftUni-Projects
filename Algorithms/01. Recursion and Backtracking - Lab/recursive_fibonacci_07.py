0a98f684399fdcb9d18b4a8d13ca393a4661b718
Desislava Pavlova <pavlova_desislava@outlook.com>
2023-04-09 16:51:56 +0300

    Initial commit

--- Algorithms/01. Recursion and Backtracking - Lab/recursive_fibonacci_07.py
+++ Algorithms/01. Recursion and Backtracking - Lab/recursive_fibonacci_07.py
@@ -1,8 +1,20 @@
-def calculate_fibonnaci(num):
+def calculate_fibonacci(num):
+    """ Fibonacci sequences with recursive solution takes too much time!"""
     if num <= 1:
         return 1
-    return calculate_fibonnaci(num - 1) + calculate_fibonnaci(num - 2)
+    return calculate_fibonacci(num - 1) + calculate_fibonacci(num - 2)
+
+
+def iterative_fibonacci(num):
+    fib_0 = 1
+    fib_1 = 1
+    result = 0
+    for _ in range(num - 1):
+        result = fib_0 + fib_1
+        fib_0, fib_1 = fib_1, result
+    return result
+
 
 num = int(input())
 
-print(calculate_fibonnaci(num))
+print(iterative_fibonnaci(num))
