6797c56776099dd397a400ceb3d4242f8d3335d8
Desislava Pavlova <pavlova_desislava@outlook.com>
2023-04-19 23:20:07 +0300

    Initial commit

---
+++ Algorithms/02. Searching and Sorting Algs - Lab/selection_sort_02.py
@@ -0,0 +1,13 @@
+def selection_sort(nums):
+    for i in range(len(nums)):
+        min_idx = i
+        for j in range(i + 1, len(nums)):
+            if nums[j] < nums[min_idx]:
+                min_idx = j
+        nums[i], nums[min_idx] = nums[min_idx], nums[i]
+
+
+nums = [int(x) for x in input().split()]
+
+selection_sort(nums)
+print(*nums, sep=' ')
