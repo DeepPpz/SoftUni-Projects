4aec4925ec32f431964bfb5ef1806458e9c16d4f
Desislava Pavlova <pavlova_desislava@outlook.com>
2023-05-01 22:35:45 +0300

    Completed

--- Fundamentals/7.3. Dictionaries - More Exercise/ranking_01.py
+++ Fundamentals/7.3. Dictionaries - More Exercise/ranking_01.py
@@ -1,8 +1,5 @@
-# not ready
-
 contests = {}
 end_results = {}
-best_results = {}
 
 while True:
     curr_contest = input().split(":")
@@ -18,36 +15,28 @@ while curr_result[0] != "end of submissions":
     username, points = curr_result[2], int(curr_result[3])
 
     if contest not in contests or password != contests[contest]:
-        curr_result = input().split("=>")
-        continue
-
-    if contest not in end_results:
-        end_results[contest] = {username: points}
-    elif username not in end_results[contest]:
-        end_results[contest][username] = points
-        best_results[username] = 0
+        pass
+    elif username not in end_results:
+        end_results[username] = {contest: points}
+    elif contest not in end_results[username]:
+        end_results[username][contest] = points
     else:
-        prev_points = end_results[contest][username]
-        end_results[contest][username] = max(prev_points, points)
+        prev_points = end_results[username][contest]
+        end_results[username][contest] = max(prev_points, points)
 
     curr_result = input().split("=>")
 
-for (lang, stud) in end_results.items():
-    for s in stud:
-        best_results[s] += sum(stud.values())
-
+best_results = {}
+for (stud, contest) in end_results.items():
+    best_results[stud] = sum(contest.values())
 rank_dict = dict(sorted(best_results.items(), key=lambda x: -x[1]))
 
-best_candidate = next(iter(rank_dict.items()))
+best_candidate = next(iter(rank_dict.items()))  # return the first key-pair value
 print(f"Best candidate is {best_candidate[0]} with total {best_candidate[1]} points.")
 print("Ranking:")
 
-for i in end_results:
-    end_results[i] = dict(sorted(end_results[i].items(), key=lambda x: x[0]))
-print(end_results)
-
-
-for (lang, stud) in end_results.items():
+sorted_results = dict(sorted(end_results.items(), key=lambda x: x[0]))
+for stud in sorted_results:
     print(f"{stud}")
-    for (k, v) in stud.items():
-        print(f"#  {lang} - {v}")
+    for (contest, points) in sorted(sorted_results[stud].items(), key=lambda x: -x[1]):
+        print(f"#  {contest} -> {points}")
