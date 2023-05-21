1c22d48265fae7fc84a8e90cd4f189a138a14e7a
Desislava Pavlova <pavlova_desislava@outlook.com>
2023-05-21 10:55:07 +0300

    Added another solution

--- Advanced/Advanced/02. Tuples and Sets - Exercise/periodic_table_03.py
+++ Advanced/Advanced/02. Tuples and Sets - Exercise/periodic_table_03.py
@@ -2,3 +2,5 @@ n = int(input())
 periodic_table = {item for _ in range(n) for item in input().split()}
 
 print(*periodic_table, sep='\n')
+
+# print(*{item for _ in range(int(input())) for item in input().split()}, sep='\n')
