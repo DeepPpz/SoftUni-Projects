cf625f083241a15e2d3d5484b29cd02b93ab541f
Desislava Pavlova <pavlova_desislava@outlook.com>
2023-05-24 12:53:24 +0300

    Initial commit

---
+++ Others/winsound_tester.py
@@ -0,0 +1,31 @@
+import winsound
+
+winsound.Beep(147, 1000)  # D3
+winsound.Beep(165, 1000)  # E3
+winsound.Beep(185, 1000)  # F#3
+winsound.Beep(165, 1000)  # E3
+winsound.Beep(147, 1000)  # D3
+
+winsound.Beep(156, 1000)  # D#3
+winsound.Beep(175, 1000)  # F3
+winsound.Beep(196, 1000)  # G3
+winsound.Beep(175, 1000)  # F3
+winsound.Beep(156, 1000)  # D#3
+
+winsound.Beep(165, 1000)  # E3
+winsound.Beep(185, 1000)  # F#3
+winsound.Beep(208, 1000)  # G#3
+winsound.Beep(185, 1000)  # F#3
+winsound.Beep(165, 1000)  # E3
+
+winsound.Beep(175, 1000)  # F3
+winsound.Beep(196, 1000)  # G3
+winsound.Beep(220, 1000)  # A3
+winsound.Beep(196, 1000)  # G3
+winsound.Beep(175, 1000)  # F3
+
+winsound.Beep(185, 1000)  # F#3
+winsound.Beep(208, 1000)  # G#3
+winsound.Beep(233, 1000)  # A#3
+winsound.Beep(208, 1000)  # G#3
+winsound.Beep(185, 1000)  # F#3
