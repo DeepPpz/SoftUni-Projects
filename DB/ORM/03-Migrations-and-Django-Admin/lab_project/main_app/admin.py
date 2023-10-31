ca472366910da0a87055157ef1a62215b06e3502
Desislava Pavlova <pavlova_desislava@outlook.com>
2023-10-31 10:43:31 +0200

    Initial commit

---
+++ DB/ORM/03-Migrations-and-Django-Admin/lab_project/main_app/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from main_app.models import Product
+
+
+# admin.site.register(Product) --> When we want to use the default admin interface
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'created_on')
+    search_fields = ('name', 'category', 'supplier')
+    list_filter = ('category', 'supplier')
+    fieldsets = (
+        ('General Information', {
+            'fields': ('name', 'description', 'price', 'barcode')
+        }),
+        ('Categorization', {
+            'fields': ('cateogory', 'supplier')
+        }),
+    )
+    date_hierarchy = ('created_on')
